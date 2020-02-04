73f2e5b49005c6edee2ab94731b4e417548c71dc
mrpsampath <mr.psampath@gmail.com>
2020-02-03 22:03:25 -0500

    new project added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Route,RouterModule} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SettingsComponent } from './settings/settings.component';
+import { NopageComponent } from './nopage/nopage.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ContactComponent } from './contact/contact.component';
 
 
-const routes: Routes = [];
+const routes: Route[] = [
+{path:''    ,redirectTo: '/home', pathMatch:'full'},
+{path: 'home',component:HomeComponent},
+{path:'settings',
+component:SettingsComponent,
+children:[
+{path:'profile',component:ProfileComponent},
+{path:'contact',component:ContactComponent},
+]
+
+},
+{path:'**',component:NopageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SettingsComponent } from './settings/settings.component';
+import { NopageComponent } from './nopage/nopage.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SettingsComponent,
+    NopageComponent,
+    ProfileComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
